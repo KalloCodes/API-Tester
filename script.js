56f5539611516fb83bbe65d165a2965d91c7756f
Dan <daniel.t72@hotmail.co.uk>
2022-05-12 20:03:37 +0100

    Wide functionality improvements
    Fixed an expansive swathe of minor issues so now stuff works properly, including break cases and managing invalid inputs. Next job is fixing layouts on different sizes of screens

--- script.js
+++ script.js
@@ -1,103 +1,142 @@
+/*Initialises a counter on every page refresh*/
 let counter = 1;
 
-    function getImages() {
-        let y = document.getElementById("quantity").value;
-        console.log("y is " + y);
-        let fetcher = "https://dog.ceo/api/breeds/image/random/" + y;
-        fetch(fetcher)
-        .then(response => response.json())
-        .then(data => processImages(data));
+function getImages() {
+    /*Gets y as the value from the input box*/
+    let y = document.getElementById("quantity").value;
+    console.log("y is " + y);
+
+    /*Escape for invalid inputs*/
+    if (y > 10 || y < 1) {
+        invalidNumber(y);
+        return;
     }
 
-    /*
-    function processImages(data) {
-        console.log(data.message);
-        var dogArray = data.message;
-        for (var URL of dogArray) {
-            var img = document.createElement("img");
-            img.src = URL;
-            document.body.appendChild(img);
-            var source = document.createElement("p");
-            source.innerHTML = URL;
-            document.body.appendChild(source);
-        }
+    /*Fetches y amount of random dog URLs*/
+    let fetcher = "https://dog.ceo/api/breeds/image/random/" + y;
+    fetch(fetcher)
+    .then(response => response.json())
+    .then(data => processImages(data));
+}
+
+/*Function for if y isn't a number between 1 and 10*/
+function invalidNumber(y) {
+    if (y>10) {
+        alert(y + " is too many!");
+    } else if (y == 0) {
+        alert("Maybe you'd prefer cats?");
+    } else {
+        alert(y + " is an invalid term");
     }
-    */
+}
 
-    function processImages(data) {
+function processImages(data) {
 
-        /*Checks if there's already a picdiv, and if so, deletes it*/
-        if (!!document.getElementById("picdiv") === true) {
-            const element = document.getElementById("picdiv");
-            element.remove();
-        }
-
-        /*Creates a new picdiv, styled as a vertical flexbox*/
-        let picbox = document.createElement("div");
-        /*
-        let x = document.getElementById("quantity").value;
-            console.log(x);
-            if (x == 1) {
-                picbox.innerText = "Finding 1 dog!";
-            } else {
-                picbox.innerText = "Finding " + x + " dogs!";
-            }
-        */
-        document.body.appendChild(picbox);
-        picbox.setAttribute("id","picdiv");
-
-    
-        var dogArray = data.message;
-        for (var URL of dogArray) {
-            var flexdiv = document.createElement("a");
-            flexdiv.setAttribute("class", "box")
-            flexdiv.setAttribute("href", URL)
-
-            /*Regex stuff? Going to try to get this working to name the dogs*/
-            let text = URL.match(/s\/.*\//);
-            let text2 = text[0].match(/\/.*\//);
-            let text3 = text2[0].match(/[^\/].*[^\/]/g);
-            let text4 = text3[0].replace("-", " ");
-            console.log(text4);
-            let reg = new RegExp(/" "/);
-            console.log(reg);
-            console.log(reg.test(text4));
-
-            /*
-            if (reg.test(text4) === true) {
-                console.log("true");
-            }
-            */
-            /*Next steps:
-            1 if the string contains a space character:
-              take the first word and the last word and swap them (with capturing groups?)
-            2 capitalise the first letters of the words
-            */
-
-            var number = document.createElement("p");
-            number.innerHTML = counter + ": " + text4;
-            var img = document.createElement("img");
-            img.src = URL;
-            flexdiv.appendChild(number);
-            flexdiv.appendChild(img);
-            document.getElementById("picdiv").appendChild(flexdiv);
-
-            var newlink = document.createElement("p");
-            newlink.innerHTML = counter + ": " + URL;
-            document.getElementById("linkdiv").appendChild(newlink);
-
-
-            counter++;
-        }
-
-        /*
-        for (var URL of dogArray) {
-            var flexdiv = document.createElement("div");
-            var number = document.createElement("p");
-            number.innerHTML = counter;
-            flex
-            var img = document.createElement("img");
-            img.src = URL;
-        }
-        */
+    /*Checks if there's already a picdiv, and if so, deletes it*/
+    if (!!document.getElementById("picdiv") === true) {
+        const element = document.getElementById("picdiv");
+        element.remove();
     }
+
+    /*Creates a new picdiv, styled as a vertical flexbox*/
+    let picbox = document.createElement("div");
+    document.body.appendChild(picbox);
+    picbox.setAttribute("id","picdiv");
+
+    /*TODO sort out ALL of this stuff. It works but. Jeez.*/
+    var dogArray = data.message;
+    for (var URL of dogArray) {
+        var flexdiv = document.createElement("a");
+        flexdiv.setAttribute("class", "box")
+        flexdiv.setAttribute("href", URL)
+
+        /*Regex stuff? Going to try to get this working to name the dogs*/
+        let text1 = URL.match(/s\/.*\//);
+        let text2 = text1[0].match(/\/.*\//);
+        let text3 = text2[0].match(/[^\/].*[^\/]/g);
+
+        let reg = new RegExp(/-/);
+        let array = [];
+
+        if (reg.test(text3) === true) {
+            array = text3[0].split("-");
+        } else {
+            array[0] = text3;
+        }
+
+        let output = [];
+
+        for(let i = 0; i<array.length; i++) {
+            array[i] = array[i].toString();
+            let splitted = array[i].split("");
+            splitted[0] = splitted[0].toUpperCase();
+            splitted = splitted.join("");
+            output.unshift(splitted);
+        }
+
+        /*Turn the output into a string*/
+        if (output.length === 2) {
+            output = output.join(" ");
+        } else {
+            output = output[0];
+        }
+
+        /*Some of the breed names aren't written right when I call them
+        in to my page. This corrects the ones I've found*/
+        switch (output) {
+            case "Blood Hound":
+                output = "Bloodhound";
+                break;
+            case "Germanshepherd":
+                output = "German Shepherd";
+                break;
+            case "Mexicanhairless":
+                output = "Mexican Hairless";
+                break;
+            case "Germanlonghair Pointer":
+                output = "German Longhair Pointer";
+                break;
+            case "Westhighland Terrier":
+                output = "West Highland Terrier";
+                break;
+            case "Stbernard":
+                output = "Saint Bernard";
+                break;
+            case "Staffordshire Bullterrier":
+                output = "Staffordshire Bull Terrier";
+                break;
+            case "Flatcoated Retriever":
+                output = "Flat-coated Retriever";
+                break;
+            case "Shihtzu":
+                output = "Shih Tzu";
+                break;
+            case "Shepherd Australian":
+                output = "Australian Shepherd";
+                break;
+            case "Bernese Mountain":
+                output = "Bernese Mountain Dog";
+                break;
+        }
+
+
+
+
+
+
+
+        var number = document.createElement("p");
+        number.innerHTML = counter + ": " + output;
+        var img = document.createElement("img");
+        img.src = URL;
+        flexdiv.appendChild(number);
+        flexdiv.appendChild(img);
+        document.getElementById("picdiv").appendChild(flexdiv);
+
+        var newlink = document.createElement("p");
+        newlink.innerHTML = counter + ": " + URL;
+        document.getElementById("linkdiv").appendChild(newlink);
+
+        counter++;
+    }
+}
